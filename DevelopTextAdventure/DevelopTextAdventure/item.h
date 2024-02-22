e2f26ceb387d2585cae303ae7f0d41988de7645d
MavenPile <s214290@students.aie.edu.au>
2024-02-22 16:45:14 +1030

    update

---
+++ DevelopTextAdventure/DevelopTextAdventure/item.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class item
+{
+private:
+
+	char* name;
+
+public:
+
+	item();
+
+	~item();
+	
+	void Description();
+
+	void Use();
+
+protected:
+
+
+
+};
+
--- DevelopTextAdventure/DevelopTextAdventure/main.cpp
+++ DevelopTextAdventure/DevelopTextAdventure/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+void CreateMap();
+
 int main()
 {
+	bool gameRunning = true;
+
+	for 
+
+	while (gameRunning)
+	{
+		std::cout << "Map: " << std::endl;
+
+
+		if (0 > 1)
+		{
+			gameRunning = false;
+		}
+	}
+
+
+	//delete[] gameMap;
+	
 	return 0;
+}
+
+void CreateMap()
+{
+	int* gameMap = new int[5];
+
 }
